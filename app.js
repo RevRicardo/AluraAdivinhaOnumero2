87445ff446a7f2071f0cd38144589304f0f7437a
Ricardo Lacerda <revricardolacerda@gmail.com>
2025-02-13 04:56:07 -0300

    implementa funções verificarChute, exibirTextoNaTela, geraNumeroAleatorio

---
+++ app.js
@@ -0,0 +1,23 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
+//Essas linhas passou para função exibirTextoNaTela
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'Jogo do número secreto';
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute() {
+    console.log(numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
